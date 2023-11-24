1f27f563ba9b95e37c0ba4e6410bc3738476b4cf
vensyyy91 <vensyyy91@yandex.ru>
2023-11-24 23:29:43 +0300

    style: checkstyle fixes

--- main-service/main-dto/src/main/java/ru/practicum/event/UpdateEventAdminRequest.java
+++ main-service/main-dto/src/main/java/ru/practicum/event/UpdateEventAdminRequest.java
@@ -1,6 +1,5 @@
 package ru.practicum.event;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +7,8 @@ import lombok.experimental.SuperBuilder;
 import ru.practicum.enums.StateActionAdmin;
 
 @SuperBuilder
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 public class UpdateEventAdminRequest extends UpdateEventRequest {
     private StateActionAdmin stateAction;
--- main-service/main-dto/src/main/java/ru/practicum/event/UpdateEventUserRequest.java
+++ main-service/main-dto/src/main/java/ru/practicum/event/UpdateEventUserRequest.java
@@ -1,6 +1,5 @@
 package ru.practicum.event;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +7,8 @@ import lombok.experimental.SuperBuilder;
 import ru.practicum.enums.StateActionUser;
 
 @SuperBuilder
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 public class UpdateEventUserRequest extends UpdateEventRequest {
     private StateActionUser stateAction;
